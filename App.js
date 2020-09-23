6801dd408bf8f998bd9b17520c78dd2fc956ee5f
Jhonatan Bergmann <jhonatan.bergmann@gmail.com>
2020-09-23 14:01:05 -0300

    Insta first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { 
+  View, 
+  Text 
+} from 'react-native'
+
+import Header from './src/components/Header'
+import Post from './src/components/Post'
+
+export default class App extends Component {
+  render() {
+    const comments = [{
+      nickname: 'Joana Elena Silva',
+      comment: 'Top'
+    }, {
+      nickname: 'Joana Elena Silva',
+      comment: 'Top'
+    }]
+
+    return(
+      <View style={{ flex: 1 }}>
+        <Header />
+        <Post image={require('./assets/imgs/fence.jpg')} 
+           comments={comments} />
+      </View>
+    )
+  }
+}
